7fdf13eb43eafe547fa09ee4d0591e255aa520c4
dem4ply <dem4ply@gmail.com>
2026-03-04 09:26:21 -0600

    se agrego la funcionalidad de crear indices de prueba usando chiibi config

--- chibi_elasticsearch/snippet.py
+++ chibi_elasticsearch/snippet.py
@@ -1,3 +1,4 @@
+from chibi.config import configuration
 from elasticsearch_dsl.connections import connections
 
 
@@ -5,18 +6,28 @@ def build_index_name( name, app_name=None, ):
     """
     crea el nombre del indice para producion o para pruebas
 
+    Para asirnar que es una prueba se usa la configuracion de chibi
+
     Parameters
     ----------
     name: str
     app_name: str
         por default toma el nombre del projecto
+
+    Example
+    -------
+    >>>from chibi.config import configuration
+    >>>configuration.elasticsearch.test_app = True
+    >>>assert build_index_name( 'hello' ) == 'test__hello'
+    >>>configuration.elasticsearch.test_app = False
+    >>>assert build_index_name( 'hello' ) == 'hello'
     """
     if app_name:
         result = f"{app_name}__{name}"
     else:
         result = name
 
-    is_test = False
+    is_test = bool( configuration.elasticsearch.test_app )
     if is_test:
         return f"test__{result}"
     return result
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,8 @@
 import unittest
+from chibi.config import configuration
 from chibi_elasticsearch.config import load_elasticsearch_config
 from chibi_elasticsearch.config import review_elasticsearch_config
+from chibi_elasticsearch.snippet import build_index_name
 
 
 class Test_config( unittest.TestCase ):
@@ -9,3 +11,15 @@ class Test_config( unittest.TestCase ):
 
     def test_review_should_be_true_if_have_config( self ):
         review_elasticsearch_config()
+
+
+class Test_build_index_name( unittest.TestCase ):
+    def test_when_setup_is_a_test_should_return_prefix_text( self ):
+        configuration.elasticsearch.test_app = True
+        result = build_index_name( 'hello' )
+        self.assertEqual( result, "test__hello" )
+
+    def test_when_it_not_setup_is_a_test_should_return_prefix_text( self ):
+        configuration.elasticsearch.test_app = False
+        result = build_index_name( 'hello' )
+        self.assertEqual( result, "hello" )
